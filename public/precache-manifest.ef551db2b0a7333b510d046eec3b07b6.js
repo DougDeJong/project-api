4da29e26d1c60d82ff79026e05c787e1dddeac1b
Doug DeJong <dougdejong@gmail.com>
2018-10-12 11:12:05 -0400

    new build

---
+++ public/precache-manifest.ef551db2b0a7333b510d046eec3b07b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ecf6dd3165f31c64d4d33006ecf5bd9",
+    "url": "/static/media/T1D3icon.4ecf6dd3.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da7664d5a4b03eea01e2",
+    "url": "/static/js/main.da7664d5.chunk.js"
+  },
+  {
+    "revision": "933cc331de4e40165526",
+    "url": "/static/js/1.933cc331.chunk.js"
+  },
+  {
+    "revision": "da7664d5a4b03eea01e2",
+    "url": "/static/css/main.142d327d.chunk.css"
+  },
+  {
+    "revision": "933cc331de4e40165526",
+    "url": "/static/css/1.9e1afc39.chunk.css"
+  },
+  {
+    "revision": "e41a8837e901d71c6a56aeb2ee4b7923",
+    "url": "/index.html"
+  }
+];
